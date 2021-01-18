e7f19d0af9e44b10ec77631434b4fc0f757e4de5
Grzegorr <grzegorzsochacki@outlook.com>
2021-01-18 16:22:09 +0000

    updae

---
+++ Run code only if its main program/example.py
@@ -0,0 +1,15 @@
+
+
+def printFunc():
+    print("Print 1")
+    print("Print 2")
+    print("Print 3")
+
+
+
+
+
+#This will not run when this script is being imported
+if __name__ == '__main__':
+    while(1):
+        printFunc()
